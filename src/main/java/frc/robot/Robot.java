6ff25cdea0ca1de34fe13c536f4790763048f246
GEM-SHS <Grant_messinger@stu.ortrail.k12.or.us>
2026-03-19 18:10:14 -0700

    minor change

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -33,7 +33,6 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
-    m_PDH.setSwitchableChannel(true);
   }
 
   /**
@@ -57,7 +56,9 @@ public class Robot extends TimedRobot {
 
   /** This function is called once each time the robot enters Disabled mode. */
   @Override
-  public void disabledInit() {}
+  public void disabledInit() {
+    m_PDH.setSwitchableChannel(true);
+  }
 
   @Override
   public void disabledPeriodic() {
